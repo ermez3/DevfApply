ddf0e5d8df40e5e791afc26414c8f317ac281b67
ermez3 <ermez3@gmail.com>
2015-06-29 04:00:44 -0500

    Devf Apply Program. message for your changes. Lines starting

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFApply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length <= 0 || args[0] == null)
+                args = new string[] { "El DevF rockea" };
+
+            ReverseString(args[0].ToString());
+        }
+
+        static void ReverseString(string data)
+        {
+            if (String.IsNullOrEmpty(data))
+                return;
+
+            char[] array = data.ToCharArray();
+            Array.Reverse(array);
+            Console.WriteLine(new String(array));
+            Console.WriteLine(data.Count(w => w.ToString().ToLower() == "e"));
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Devf Apply
+C# Program that solves two problems:
+  - Reverse a string
+  - Count the numbers of e char that appears in a string.
